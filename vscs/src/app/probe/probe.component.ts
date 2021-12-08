7780c5ca33a7ed16e0cead5366352392e28af03f
赵华明 <475484467@qq.com>
2021-12-08 16:05:12 +0800

    no message

--- vscs/src/app/probe/probe.component.ts
+++ vscs/src/app/probe/probe.component.ts
@@ -1,17 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NcEventService} from 'noce/core';
 
 @Component({
   templateUrl: './probe.component.html',
   styleUrls: ['./probe.component.less']
 })
-export class ProbeComponent implements OnInit {
+export class ProbeComponent implements OnInit, OnDestroy {
   edit = '';
+  searchEvent: any; // 搜索事件
 
   constructor(private event: NcEventService) {
-    this.event.on('SEARCH').subscribe(keyword => {
+    // 订阅搜索事件
+    this.searchEvent = this.event.on('SEARCH').subscribe(keyword => {
       alert(keyword);
     })
+  }
+
+  ngOnDestroy(): void {
+    // 取消订阅搜索事件
+    this.searchEvent.unsubscribe();
   }
 
   ngOnInit(): void {
