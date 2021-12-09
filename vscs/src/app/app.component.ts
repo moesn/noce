799401f3713699c8e3989ac02ea640719ee7efb7
赵华明 <475484467@qq.com>
2021-12-09 17:56:35 +0800

    no message

--- vscs/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'vscs-root',
-  template: '<router-outlet></router-outlet>'
-})
-export class AppComponent {
-  title = 'vscs';
-}
