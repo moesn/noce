1c6b48547112d7ce16146a39ace13aac5b5cfe42
赵华明 <475484467@qq.com>
2022-01-05 16:06:07 +0800

    no message

--- play/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'play3-root',
-  template: '<router-outlet></router-outlet>'
-})
-export class AppComponent {
-  title = 'play3';
-}
